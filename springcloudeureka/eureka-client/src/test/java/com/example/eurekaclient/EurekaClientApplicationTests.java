8cea8ea38de39bb655a66ca806cc09f30fa503ab
keweizhou <1514991931@qq.com>
2020-07-05 00:15:41 +0800

    application tests

--- springcloudeureka/eureka-client/src/test/java/com/example/eurekaclient/EurekaClientApplicationTests.java
+++ springcloudeureka/eureka-client/src/test/java/com/example/eurekaclient/EurekaClientApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.eurekaclient;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class EurekaClientApplicationTests {
+public class EurekaClientApplicationTests {
 
     @Test
     void contextLoads() {
