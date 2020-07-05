ef4014f4742ac43f3955b71110bdb6fb9340be9d
keweizhou <1514991931@qq.com>
2020-07-05 12:35:17 +0800

    communication by feign rpc

---
+++ springcloudeureka/service-feign/src/main/java/com/example/servicefeign/controller/HelloFeignController.java
@@ -0,0 +1,26 @@
+package com.example.servicefeign.controller;
+
+import com.example.servicefeign.feign.FeignService;
+import com.netflix.discovery.converters.Auto;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description
+ * Date 2020/7/5 12:31
+ * Created by kwz
+ */
+@RestController
+public class HelloFeignController {
+
+    @Setter(onMethod_ = @Autowired)
+    private FeignService feignService;
+
+    @GetMapping("feignHello")
+    public String sayHello(@RequestParam("name") String name) {
+        return feignService.sayHelloFromEurekaClientOne(name);
+    }
+}
