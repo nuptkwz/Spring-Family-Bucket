9025cad63efac22e17f6aab616cc625077572ed0
keweizhou <1514991931@qq.com>
2020-07-05 12:56:59 +0800

    fix spring boot test

--- springcloudeureka/service-feign/src/test/java/com/example/servicefeign/ServiceFeignApplicationTests.java
+++ springcloudeureka/service-feign/src/test/java/com/example/servicefeign/ServiceFeignApplicationTests.java
@@ -1,13 +1,17 @@
 package com.example.servicefeign;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-class ServiceFeignApplicationTests {
+@RunWith(SpringRunner.class)
+public class ServiceFeignApplicationTests {
+
 
     @Test
-    void contextLoads() {
+    private void contextLoads() {
     }
 
 }
